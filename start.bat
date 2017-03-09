2593c3014fc37574db5cb7aa70040340555cadff
Beowulfv <beowolfv@gmail.com>
2017-03-08 23:58:40 -0500

    Add files via upload

---
+++ start.bat
@@ -0,0 +1,17 @@
+@echo off
+:start
+C:\Windows\System32\tasklist /FI "IMAGENAME eq arma3server.exe" 2>NUL | C:\Windows\System32\find /I /N "arma3server.exe">NUL
+if "%ERRORLEVEL%"=="0" goto loop
+echo Server monitored is not running, will be started now 
+start "" /wait "C:\EDIT_ME\arma3server.exe" -config=C:\EDIT_ME\@ExileServer\config.cfg -cfg=C:\EDIT_ME\@ExileServer\basic.cfg -profiles=sc -name=sc -port=2302 -autoinit -bepath=C:\EDIT_ME\BattlEye -servermod=@ExileServer; -mod=@Exile;Kart,Mark,Heli;
+echo Server started succesfully
+goto started
+:loop
+cls
+echo Server is already running, running monitoring loop
+:started
+C:\Windows\System32\timeout /t 10
+C:\Windows\System32\tasklist /FI "IMAGENAME eq arma3server.exe" 2>NUL | C:\Windows\System32\find /I /N "arma3server.exe">NUL
+if "%ERRORLEVEL%"=="0" goto loop
+C:\Windows\System32\taskkill /im arma3server.exe
+goto start
